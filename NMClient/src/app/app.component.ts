079db2a0472e707eae97084fa34aa83bb96decb1
nicklasvsams <nicklasvsams@gmail.com>
2023-03-21 14:19:24 +0100

    Test of the flightService

--- NMClient/src/app/app.component.ts
+++ NMClient/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlightService } from './_services/flight.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'NM Flights';
+  accessToken = {};
 
-  
+  constructor(private _flightService: FlightService) {}
+
+  ngOnInit(): void{
+    this._flightService.getAccessToken().subscribe(x => {this.accessToken = x; console.log(x)});
+  }
 }
