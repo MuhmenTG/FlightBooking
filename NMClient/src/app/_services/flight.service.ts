c99f73ade49812dd4eaa9af1228fa5e8709384ac
nicklasvsams <nicklasvsams@gmail.com>
2023-03-21 14:19:01 +0100

    Test API call to get access_token from Amadeus

--- NMClient/src/app/_services/flight.service.ts
+++ NMClient/src/app/_services/flight.service.ts
@@ -13,17 +13,17 @@ export class FlightService {
   private amadeusUrl = environment.amadeusApiUrl;
 
   private httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-    params: new HttpParams()
-      .set('grant_type', 'client_credentials')
-      .set('client_id', 'xlUodVi30L0U8snyBsa1qenY4BNyUjMA')
-      .set('client_secret', 'A2GpGXyewfl0G3gu')
+    headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' })
   }
+
+  private body = "grant_type=client_credentials&client_id=73Vz7GstLSf9xaHWx0fPMH6PRg6wYqjT&client_secret=lSJEDh6AwHwH5omy"
 
   constructor(private http: HttpClient) { }
 
-  getAccessToken(): Observable<Flight[]> {
-    return this.http.get<Flight[]>(this.amadeusUrl, this.httpOptions);
+  getAccessToken(): Observable<{}> {
+    return this.http.post<{}>(this.amadeusUrl, 
+    this.body, 
+    this.httpOptions);
   }
 
   getFlights(): Observable<Flight[]> {
