bc0e52112e53762423167ef5fb142ba435ff61a4
nicklasvsams <nicklasvsams@gmail.com>
2023-03-21 14:46:59 +0100

    Refactored access token call to use HttpParams

--- NMClient/src/app/_services/flight.service.ts
+++ NMClient/src/app/_services/flight.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
+import { AccessTokenResponse } from '../_models/accessTokenResponse';
 import { Flight } from '../_models/flight';
 
 @Injectable({
@@ -12,18 +13,15 @@ export class FlightService {
   private apiUrl = environment.apiUrl + "/flight";
   private amadeusUrl = environment.amadeusApiUrl;
 
-  private httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' })
-  }
-
-  private body = "grant_type=client_credentials&client_id=73Vz7GstLSf9xaHWx0fPMH6PRg6wYqjT&client_secret=lSJEDh6AwHwH5omy"
-
+  private accessTokenParameters = new HttpParams()
+      .set('grant_type', 'client_credentials')
+      .set('client_id', '73Vz7GstLSf9xaHWx0fPMH6PRg6wYqjT')
+      .set('client_secret', 'lSJEDh6AwHwH5omy')
+  
   constructor(private http: HttpClient) { }
 
-  getAccessToken(): Observable<{}> {
-    return this.http.post<{}>(this.amadeusUrl, 
-    this.body, 
-    this.httpOptions);
+  getAccessToken(): Observable<AccessTokenResponse> {
+    return this.http.post<AccessTokenResponse>(this.amadeusUrl, this.accessTokenParameters);
   }
 
   getFlights(): Observable<Flight[]> {
