8189132e5389cbc1662d910e323e81069bedad31
nicklasvsams <nicklasvsams@gmail.com>
2023-03-26 16:16:59 +0200

    Refactored service to allow api calls

--- NMClient/src/app/_services/flight.service.ts
+++ NMClient/src/app/_services/flight.service.ts
@@ -2,9 +2,10 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
-import { AccessTokenResponse } from '../_models/accessTokenResponse';
-import { Flight } from '../_models/flight';
-import { SearchFlight } from '../_models/searchFlight';
+import { AccessTokenResponse } from '../_models/AccessTokenResponse';
+import { SearchFlightsResponse } from '../_models/SearchFlightsResponse';
+import { SearchFlightsRequest } from '../_models/SearchFlightsRequest';
+import { CustomerInfo } from '../_models/CustomerInfo';
 
 @Injectable({
   providedIn: 'root'
@@ -15,32 +16,39 @@ export class FlightService {
   private amadeusUrl = environment.amadeusApiUrl;
 
   private accessTokenParameters = new HttpParams()
-      .set('grant_type', 'client_credentials')
-      .set('client_id', '73Vz7GstLSf9xaHWx0fPMH6PRg6wYqjT')
-      .set('client_secret', 'lSJEDh6AwHwH5omy')
+    .set('grant_type', 'client_credentials')
+    .set('client_id', '73Vz7GstLSf9xaHWx0fPMH6PRg6wYqjT')
+    .set('client_secret', 'lSJEDh6AwHwH5omy')
 
+  private body: SearchFlightsRequest = { originLocationCode: "AXX", destinationLocationCode: "YYZ", departureDate: "2023-04-15", returnDate: "2023-04-21", adults: 1 };
 
-  private token: AccessTokenResponse = {access_token: ''};
-  private body: SearchFlight = {originLocationCode: "AXX", destinationLocationCode: "YYZ", departureDate: "2023-04-15", returnDate:"2023-04-21", adults: 1};
+  private httpOptions = {};
 
-  private httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Bearer Wm9oLBBQagCVxMhTbZAa6D05k0sg'
-    })
-  };
-  
   constructor(private http: HttpClient) { }
 
-  getAccessToken(): void {
-    this.http.post<AccessTokenResponse>(this.amadeusUrl, this.accessTokenParameters).subscribe(x => 
-      {
-        this.token.access_token = x.access_token; 
-        this.getFlights()
-      });
+  setHttpOptions(): void {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': "Bearer " + environment.access_token
+      })
+    }
   }
 
-  getFlights(): Observable<Flight[]> {
-    return this.http.post<Flight[]>(this.apiUrl + "/searchFlight", this.body, this.httpOptions);
+  getAccessToken(): Observable<AccessTokenResponse> {
+    return this.http.post<AccessTokenResponse>(this.amadeusUrl, this.accessTokenParameters);
+  }
+  // searchCriteria: SearchFlightsRequest
+  getFlights(): Observable<SearchFlightsResponse[]> {
+    console.log(this.httpOptions);
+    return this.http.post<SearchFlightsResponse[]>(this.apiUrl + "/searchFlights", this.body, this.httpOptions);
+  }
+
+  getFlightInfo(flightOffer: SearchFlightsResponse): Observable<{}> {
+    return this.http.post<{}>(this.apiUrl + "/selectFlight", flightOffer, this.httpOptions);
+  }
+
+  getFlightConfirmation(customerInfo: CustomerInfo): Observable<SearchFlightsResponse[]> {
+    return this.http.post<SearchFlightsResponse[]>(this.apiUrl + "/confirmFlight", customerInfo, this.httpOptions);
   }
 }
