44df8d94f706ebe5745df909ff7acbeea10ef46a
nicklasvsams <nicklasvsams@gmail.com>
2023-03-26 16:16:40 +0200

    added environment variable

--- NMClient/src/environments/environment.development.ts
+++ NMClient/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: "http://localhost:8000/api",
-  amadeusApiUrl: "https://test.api.amadeus.com/v1/security/oauth2/token"
+  amadeusApiUrl: "https://test.api.amadeus.com/v1/security/oauth2/token",
+  access_token: "initial value"
 };
