9a038a7d34e8800c7c79a83547d2af90caac84d5
nicklasvsams <nicklasvsams@gmail.com>
2023-03-26 16:15:19 +0200

    added new component

--- NMClient/src/app/app.module.ts
+++ NMClient/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFlightsComponent } from './search-flights/search-flights.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFlightsComponent
   ],
   imports: [
     BrowserModule,
