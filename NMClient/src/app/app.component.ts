0c79c769ba4e5e54049bf5fcf2e22523470fe60c
nicklasvsams <nicklasvsams@gmail.com>
2023-03-26 16:14:55 +0200

    Setup for testing backend

--- NMClient/src/app/app.component.ts
+++ NMClient/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FlightService } from './_services/flight.service';
-import { AccessTokenResponse } from './_models/accessTokenResponse';
+import { AccessTokenResponse } from './_models/AccessTokenResponse';
+import { environment } from 'src/environments/environment.development';
 
 
 @Component({
@@ -8,14 +9,19 @@ import { AccessTokenResponse } from './_models/accessTokenResponse';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'NM Flights';
-  accessToken: AccessTokenResponse = { access_token: ""};
+  accessToken: AccessTokenResponse = { access_token: "" };
 
-  constructor(private _flightService: FlightService) {}
+  constructor(private _flightService: FlightService) { }
 
-  ngOnInit(): void{
-    this._flightService.getAccessToken();
-    this._flightService.getFlights().subscribe(x => {console.log(x)});
+  async ngOnInit(): Promise<void> {
+    this._flightService.getAccessToken().subscribe(x => {
+      environment.access_token = x.access_token;
+      this._flightService.setHttpOptions();
+      this._flightService.getFlights().subscribe(y => console.log(y))
+    });
+
+    // this._flightService.getFlights().subscribe(x => {console.log(x)});
   }
 }
