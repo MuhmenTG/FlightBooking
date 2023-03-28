2e2c9924877ca4b25316eaf1b00a63c80635414b
MuhmenTG <mpe@nota.dk>
2023-03-28 13:27:01 +0200

    commit

--- bookingportal/app/Http/Controllers/FlightBookingController.php
+++ bookingportal/app/Http/Controllers/FlightBookingController.php
@@ -87,35 +87,6 @@ class FlightBookingController extends Controller
 
     public function selectFlightOffer(Request $request) 
     {
-        $validator = Validator::make($request->all(), [
-            'type' => 'required|string|in:flight-offer',
-            'id' => 'required|string',
-            'source' => 'required|string|in:GDS',
-            'instantTicketingRequired' => 'required|boolean',
-            'nonHomogeneous' => 'required|boolean',
-            'oneWay' => 'required|boolean',
-            'lastTicketingDate' => 'required|date_format:Y-m-d',
-            'numberOfBookableSeats' => 'required|integer|min:1',
-            'itineraries.*.duration' => 'required|string',
-            'itineraries.*.segments.*.departure.iataCode' => 'required|string|size:3',
-            'itineraries.*.segments.*.departure.terminal' => 'string',
-            'itineraries.*.segments.*.departure.at' => 'required|date_format:Y-m-d\TH:i:s',
-            'itineraries.*.segments.*.arrival.iataCode' => 'required|string|size:3',
-            'itineraries.*.segments.*.arrival.terminal' => 'string',
-            'itineraries.*.segments.*.arrival.at' => 'required|date_format:Y-m-d\TH:i:s',
-            'itineraries.*.segments.*.carrierCode' => 'required|string',
-            'itineraries.*.segments.*.number' => 'required|string',
-            'itineraries.*.segments.*.aircraft.code' => 'required|string',
-            'itineraries.*.segments.*.operating.carrierCode' => 'required|string',
-            'itineraries.*.segments.*.duration' => 'required|string',
-            'itineraries.*.segments.*.id' => 'required|string',
-            'itineraries.*.segments.*.numberOfStops' => 'required|integer|min:0',
-            'itineraries.*.segments.*.blacklistedInEU' => 'required|boolean',
-            'price.currency' => 'required|string|size:3',
-            'price.total' => 'required|string',
-            'price.base' => 'required|string',
-            'price.fees.*.amount' => 'required',
-        ]);
         $url = 'https://test.api.amadeus.com/v1/shopping/flight-offers/pricing';
 
         $jsonFlightData = $request->json()->all();
@@ -130,8 +101,9 @@ class FlightBookingController extends Controller
         $accessTtoken = 'hSj7NAANBJCM1TgKww3GuKp8le66';
 
         $response = $this->httpRequest($url, $accessTtoken, "post", $data);
-
-        return $response;
+        if($response){
+            return $response;
+        }
     }
 
     public function flightConfirmation(Request $request)
@@ -158,7 +130,7 @@ class FlightBookingController extends Controller
         
         $flightSegments = BookingFactory::createFlightBookingRecord($flightData, $bookingReferenceNumber);
 
-        
+         
         $booking = [
             'success' => true,
             'PAX'  => $passengers,
