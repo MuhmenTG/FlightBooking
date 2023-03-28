4f033a3f5b367977b37f3ae8e67e26432d728fd9
nicklasvsams <nicklasvsams@gmail.com>
2023-03-28 16:26:23 +0200

    Fixed API connection to backend and further work

--- NMClient/src/app/_services/flight.service.ts
+++ NMClient/src/app/_services/flight.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 import { AccessTokenResponse } from '../_models/AccessTokenResponse';
-import { SearchFlightsResponse } from '../_models/SearchFlightsResponse';
+import { SearchFlightsResponses } from '../_models/SearchFlightsResponses';
 import { SearchFlightsRequest } from '../_models/SearchFlightsRequest';
 import { CustomerInfo } from '../_models/CustomerInfo';
 
@@ -20,8 +20,6 @@ export class FlightService {
     .set('client_id', '73Vz7GstLSf9xaHWx0fPMH6PRg6wYqjT')
     .set('client_secret', 'lSJEDh6AwHwH5omy')
 
-  private body: SearchFlightsRequest = { originLocationCode: "AXX", destinationLocationCode: "YYZ", departureDate: "2023-04-15", returnDate: "2023-04-21", adults: 1 };
-
   private httpOptions = {};
 
   constructor(private http: HttpClient) { }
@@ -29,7 +27,7 @@ export class FlightService {
   setHttpOptions(): void {
     this.httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json',
         'Authorization': "Bearer " + environment.access_token
       })
     }
@@ -38,17 +36,16 @@ export class FlightService {
   getAccessToken(): Observable<AccessTokenResponse> {
     return this.http.post<AccessTokenResponse>(this.amadeusUrl, this.accessTokenParameters);
   }
-  // searchCriteria: SearchFlightsRequest
-  getFlights(): Observable<SearchFlightsResponse[]> {
-    console.log(this.httpOptions);
-    return this.http.post<SearchFlightsResponse[]>(this.apiUrl + "/searchFlights", this.body, this.httpOptions);
+
+  getFlights(body: SearchFlightsRequest): Observable<SearchFlightsResponses> {
+    return this.http.post<SearchFlightsResponses>(this.apiUrl + "/searchFlights", JSON.stringify(body), this.httpOptions);
   }
 
-  getFlightInfo(flightOffer: SearchFlightsResponse): Observable<{}> {
-    return this.http.post<{}>(this.apiUrl + "/selectFlight", flightOffer, this.httpOptions);
+  getFlightInfo(flightOffer: {}): Observable<{}> {
+    return this.http.post<{}>(this.apiUrl + "/chooseFlightOffer", JSON.stringify(flightOffer), this.httpOptions);
   }
 
-  getFlightConfirmation(customerInfo: CustomerInfo): Observable<SearchFlightsResponse[]> {
-    return this.http.post<SearchFlightsResponse[]>(this.apiUrl + "/confirmFlight", customerInfo, this.httpOptions);
+  getFlightConfirmation(customerInfo: CustomerInfo): Observable<SearchFlightsResponses[]> {
+    return this.http.post<SearchFlightsResponses[]>(this.apiUrl + "/confirmFlight", JSON.stringify(customerInfo), this.httpOptions);
   }
 }
