558011eae132f86c1169029373844e10995cf677
nicklasvsams <nicklasvsams@gmail.com>
2023-03-29 16:37:40 +0200

    Unfinished testing - Bad practice - Don't do this

--- NMClient/src/app/show-flightoffers/show-flightoffers.component.ts
+++ NMClient/src/app/show-flightoffers/show-flightoffers.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CustomerInfo } from '../_models/CustomerInfo';
+import { FlightInfoResponse } from '../_models/Flights/FlightInfoResponse';
+import { FlightResponse } from '../_models/Flights/FlightResponse';
+import { SearchFlightsResponses } from '../_models/Flights/SearchFlightsResponses';
+import { FlightService } from '../_services/flight.service';
 
 @Component({
   selector: 'app-show-flightoffers',
@@ -6,5 +11,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./show-flightoffers.component.css']
 })
 export class ShowFlightoffersComponent {
+  @Input() offers!: FlightResponse[];
+  flightInfo = {} as FlightInfoResponse
+  customerInfo: CustomerInfo = { firstName: "Bent", lastName: "Bentesen", email: "test@email.com", dateOfBirth: Date(), passengerType: "Adult" }
 
+  constructor(private _flightService: FlightService) { }
+
+  chooseFlight(id: string) {
+    this._flightService.getFlightInfo(this.offers[parseInt(id)]).subscribe(info => {
+      info.data.flightOffers[0].passengers = [];
+      info.data.flightOffers[0].passengers.push(this.customerInfo);
+      this._flightService.getFlightConfirmation(info.data.flightOffers[0]).subscribe(x => console.log(JSON.stringify(x)))
+    })
+  }
 }
