a09ad1d28b6d5733085db594f32778927f7b188b
nicklasvsams <nicklasvsams@gmail.com>
2023-04-25 14:59:59 +0200

    Setup and initial work on search-hotels component

---
+++ NMClient/src/app/search-hotels/search-hotels.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { HotelService } from '../_services/hotel.service';
+import { NgForm } from '@angular/forms';
+import { SearchHotelsRequest } from '../_models/Hotels/SearchHotelsRequest';
+import { SearchHotelsResponses } from '../_models/Hotels/SearchHotelsResponses';
+import { HotelResponse } from '../_models/Hotels/HotelResponse';
+
+@Component({
+  selector: 'app-search-hotels',
+  templateUrl: './search-hotels.component.html',
+  styleUrls: ['./search-hotels.component.css']
+})
+export class SearchHotelsComponent {
+  peopleAmount = [1, 2, 3, 4, 5];
+  roomAmount = ["1", "2", "3", "4"];
+  model: SearchHotelsRequest = { cityCode: '', adults: this.peopleAmount[0], checkInDate: new Date(), checkOutDate: new Date(), roomQuantity: this.roomAmount[0], priceRange: '', paymentPolicy: '', boardType: '' }
+  hotelResponses: HotelResponse[] = [];
+  formSubmitted = false;
+  todayDate = Date.now();
+
+  constructor(private _hotelService: HotelService) { }
+
+  submitForm(form: NgForm) {
+    if (!form.valid) {
+      return alert("Form is not valid. Try again.");
+    } else {
+      this._hotelService.getHotels(this.model).subscribe(responses => {
+        this.hotelResponses = [];
+        responses.data.forEach(hotelOffer => {
+          this.hotelResponses.push(hotelOffer);
+          this.formSubmitted = true;
+        });
+        return true;
+      })
+      return true;
+    }
+  }
+}
