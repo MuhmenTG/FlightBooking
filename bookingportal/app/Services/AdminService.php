d572da172332a51bc7912b88dc8383770b95a924
MuhmenTG <mpe@nota.dk>
2023-04-30 19:48:49 +0200

    added new controller and changed  name

--- bookingportal/app/Services/AdminService.php
+++ bookingportal/app/Services/AdminService.php
@@ -1,11 +1,16 @@
 <?php
+
+declare(strict_types=1);
 namespace App\Services;
 
-use App\Models\User;
+use App\Models\Faq;
+use App\Models\FlightBooking;
+use App\Models\PassengerInfo;
 use App\Models\UserAccount;
+use App\Models\UserEnquiry;
 use Illuminate\Support\Facades\Hash;
 
-class AdminService {
+class BackOfficeService {
 
     public static function createOrEditAgent(string $firstName, string $lastName, string $email, string $status, int $isAdmin, int $isAgent, $userId = null) : UserAccount
     {
@@ -57,6 +62,64 @@ class AdminService {
         return false;
     }
 
+    public static function createOrUpdateFaq(string $question, string $answer, int $faqId = null) : Faq
+    {
+        if($faqId && $faqId !== null){
+            $faq = Faq::byId($faqId)->first();
+            if (!$faq) {
+                return false;
+            }
+        }
+        $faq = new Faq();
 
-    
+        $faq->setQuestion($question);
+        $faq->setAnswer($answer);
+        $faq->save();
+        return $faq;
+    }
+
+    public static function findBookingByReference(string $bookingReference): ?FlightBooking
+    {
+        $bookedFlightSegments = FlightBooking::where(FlightBooking::COL_BOOKINGREFERENCE, $bookingReference)->get();
+        $bookedFlightPassenger = PassengerInfo::where(PassengerInfo::COL_PNR, $bookingReference)->get();
+        
+        if (!$bookedFlightSegments->isEmpty() && !$bookedFlightPassenger->isEmpty()) {
+            return FlightBooking::ByBookingReference($bookingReference)->first();
+        }
+        
+        return null;
+    }
+
+    public static function cancelBooking(FlightBooking $booking): ?FlightBooking
+    {
+        $booking->setAttribute(FlightBooking::COL_ISCANCELLED, 1);
+        $isflightSegmentsCancelled = $booking->save();
+        
+        if (!$isflightSegmentsCancelled) {
+            return null;
+        }
+        
+        return FlightBooking::ByBookingReference($booking->getAttribute(FlightBooking::COL_BOOKINGREFERENCE))->ByIsCancelled(1)->get();
+    }
+
+    public static function cancelBookingPassengers(FlightBooking $booking): ?PassengerInfo
+    {
+        $isPassengersCancelled = PassengerInfo::where(PassengerInfo::COL_PNR, $booking->getAttribute(FlightBooking::COL_BOOKINGREFERENCE))
+            ->update([PassengerInfo::COL_ISCANCELLED => 1]);
+        
+        if (!$isPassengersCancelled) {
+            return null;
+        }
+        
+        return PassengerInfo::ByBookingReference($booking->getAttribute(FlightBooking::COL_BOOKINGREFERENCE))->ByIsCancelled(1)->get();
+    }
+
+    public static function findUserEnquiryById(int $id): ?UserEnquiry
+    {
+        $userEnquiry = UserEnquiry::ById($id);
+        if($userEnquiry){
+            return $userEnquiry;
+        }
+        return false;
+    }
 }
