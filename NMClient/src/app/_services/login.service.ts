03510a72f30a0b0b3167e006d856d8be257d4605
Nicklas Sams <nicklasvsams@gmail.com>
2023-05-16 08:55:33 +0200

    Work on login service

--- NMClient/src/app/_services/login.service.ts
+++ NMClient/src/app/_services/login.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { LoginRequest } from '../_models/Employees/LoginRequest';
+import { Observable } from 'rxjs';
+import { LoginResponse } from '../_models/Employees/LoginResponse';
+import { LogoutRequest } from '../_models/Employees/LogoutRequest';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private apiUrl = environment.apiUrl + "/auth";
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  constructor(private http: HttpClient) { }
+
+  authenticateLogin(body: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(this.apiUrl + "/login", JSON.stringify(body), this.httpOptions);
+  }
+
+  authenticateLogout(body: LogoutRequest): Observable<void> {
+    return this.http.post<void>(this.apiUrl + "/logout", JSON.stringify(body), this.httpOptions);
+  }
 }
