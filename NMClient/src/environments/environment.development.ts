ea76c168ceced806bef934539b5e17172a8a6fef
Nicklas Sams <nicklasvsams@gmail.com>
2023-05-16 08:57:03 +0200

    Removed access token from env variables

--- NMClient/src/environments/environment.development.ts
+++ NMClient/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: "http://localhost:8000/api",
   amadeusApiUrl: "https://test.api.amadeus.com/v1/",
-  access_token: "initial value"
+  // access_token: "initial value"
 };
