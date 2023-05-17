189a511b8d231e5367e81daf1393b22b55bf8b6a
Nicklas Sams <nicklasvsams@gmail.com>
2023-05-17 14:28:53 +0200

    login changes - needs fixing duh

--- NMClient/src/app/login/login.component.ts
+++ NMClient/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../_services/login.service';
+import { NgForm } from '@angular/forms';
+import { LoginRequest } from '../_models/Employees/LoginRequest';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  model: LoginRequest = {email: "", password: ""};
 
+  constructor(private _loginService: LoginService){};
+
+  submitForm(form: NgForm) {
+
+    if (!form.valid) {
+      return alert("Form is not valid. Try again.");
+    } else {
+      this._flightService.getFlights(this.model).subscribe(response => {
+        this.flightsResponses = response;
+        this.findAllUniqueCarrierCodes();
+        this.swapCarrierCodeForCompanyName();
+      })
+    }
+  }
 }
