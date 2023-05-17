ebbfc5fae253ef58db9630df0558e14055db8f1c
Nicklas Sams <nicklasvsams@gmail.com>
2023-05-17 13:43:26 +0200

    Implemented pagination functionality

--- NMClient/src/app/show-flightoffers/show-flightoffers.component.ts
+++ NMClient/src/app/show-flightoffers/show-flightoffers.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FlightInfoResponse } from '../_models/Flights/FlightInfoResponse';
-import { FlightResponses } from '../_models/Flights/FlightResponse';
+import { FlightResponses } from '../_models/Flights/FlightResponses';
 import { FlightService } from '../_services/flight.service';
 
 @Component({
@@ -8,16 +8,21 @@ import { FlightService } from '../_services/flight.service';
   templateUrl: './show-flightoffers.component.html',
   styleUrls: ['./show-flightoffers.component.css']
 })
-export class ShowFlightoffersComponent {
+export class ShowFlightoffersComponent implements OnInit{
   @Input() offers!: FlightResponses;
   @Input() formSubmitted!: boolean;
   flightInfo = {} as FlightInfoResponse;
   flightChosen: boolean = false;
   moreOffers: boolean = true;
   shownOffers: number = 10;
+  private paginationCount: number = 10;
 
 
   constructor(private _flightService: FlightService) { }
+
+  ngOnInit(): void {
+    this.offers.data.length > this.paginationCount ? this.moreOffers == true : this.moreOffers == false;
+  }
 
   chooseFlight(id: string) {
     this._flightService.getFlightInfo(this.offers.data[parseInt(id) - 1]).subscribe(info => {
@@ -26,9 +31,17 @@ export class ShowFlightoffersComponent {
     })
   }
 
-
+  showMoreOffers(){
+    if (this.shownOffers + this.paginationCount > this.offers.data.length) {
+      this.shownOffers = this.offers.data.length;
+      this.moreOffers = false;
+    }
+    else this.shownOffers += this.paginationCount;
+  }
 
   reset() {
     this.flightChosen = false;
+    this.shownOffers = this.paginationCount;
+    this.moreOffers = true;
   }
 }
