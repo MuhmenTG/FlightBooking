54e416e52b9be3c092f5c2227bd4f9e0ad1b9449
Nicklas Sams <nicklasvsams@gmail.com>
2023-05-17 09:33:47 +0200

    Work on pagination

--- NMClient/src/app/show-flightoffers/show-flightoffers.component.ts
+++ NMClient/src/app/show-flightoffers/show-flightoffers.component.ts
@@ -1,9 +1,7 @@
-import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
-import { CustomerInfo } from '../_models/CustomerInfo';
+import { Component, Input } from '@angular/core';
 import { FlightInfoResponse } from '../_models/Flights/FlightInfoResponse';
-import { FlightResponse, FlightResponses } from '../_models/Flights/FlightResponse';
+import { FlightResponses } from '../_models/Flights/FlightResponse';
 import { FlightService } from '../_services/flight.service';
-import { Carrier } from '../_models/Flights/Carrier';
 
 @Component({
   selector: 'app-show-flightoffers',
@@ -13,8 +11,10 @@ import { Carrier } from '../_models/Flights/Carrier';
 export class ShowFlightoffersComponent {
   @Input() offers!: FlightResponses;
   @Input() formSubmitted!: boolean;
-  flightInfo = {} as FlightInfoResponse
+  flightInfo = {} as FlightInfoResponse;
   flightChosen: boolean = false;
+  moreOffers: boolean = true;
+  shownOffers: number = 10;
 
 
   constructor(private _flightService: FlightService) { }
@@ -23,22 +23,12 @@ export class ShowFlightoffersComponent {
     this._flightService.getFlightInfo(this.offers.data[parseInt(id) - 1]).subscribe(info => {
       this.flightChosen = true;
       this.flightInfo = info;
-
     })
   }
 
+
+
   reset() {
     this.flightChosen = false;
-  }
-
-  getCarrier(carrierCode: string) {
-    let carrier: Carrier;
-    // DANGEROUS CODE - DO NOT USE
-    // this._flightService.getCarrier(carrierCode).subscribe(x => {
-    //   x.forEach(carrierInfo => {
-    //     carrier = carrierInfo;
-    //     return carrier.commonName;
-    //   });
-    // });
   }
 }
