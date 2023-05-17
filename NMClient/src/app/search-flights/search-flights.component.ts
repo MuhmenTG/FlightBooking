b4130f76b76f83c3110f0984356689ea27e8b020
Nicklas Sams <nicklasvsams@gmail.com>
2023-05-17 12:33:15 +0200

    Functionality to reset search data on new search

--- NMClient/src/app/search-flights/search-flights.component.ts
+++ NMClient/src/app/search-flights/search-flights.component.ts
@@ -1,7 +1,7 @@
-import { Component, ViewChildren } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ShowFlightoffersComponent } from '../show-flightoffers/show-flightoffers.component';
-import { FlightResponse, FlightResponses } from '../_models/Flights/FlightResponse';
+import { FlightResponses } from '../_models/Flights/FlightResponses';
 import { SearchFlightsRequest } from '../_models/Flights/SearchFlightsRequest';
 import { FlightService } from '../_services/flight.service';
 import { CarrierCodesResponse } from '../_models/Flights/CarrierCodesResponse';
@@ -12,13 +12,13 @@ import { CarrierCodesResponse } from '../_models/Flights/CarrierCodesResponse';
   styleUrls: ['./search-flights.component.css']
 })
 export class SearchFlightsComponent {
-  @ViewChildren('ShowFlightoffersComponent') child!: ShowFlightoffersComponent;
+  @ViewChild(ShowFlightoffersComponent) child!: ShowFlightoffersComponent;
   classes = ['First class', 'Business class', 'Economy class']
   adults = [1, 2, 3, 4, 5]
   carrierCodes: String[] = [];
   carrierCodeResponse: CarrierCodesResponse = {data: []};
   model: SearchFlightsRequest = { travelType: 0, originLocationCode: '', destinationLocationCode: '', departureDate: '', returnDate: '', adults: this.adults[0], class: this.classes[0] }
-  flightsResponses: FlightResponses = {data: []};
+  flightsResponses: FlightResponses = {count: 0, data: []};
   formSubmitted = false;
   todayDate = Date.now();
 
@@ -29,6 +29,11 @@ export class SearchFlightsComponent {
   }
 
   submitForm(form: NgForm) {
+    if (this.formSubmitted) {
+      this.formSubmitted = false;
+      this.resetAll();
+    }
+
     if (!form.valid) {
       return alert("Form is not valid. Try again.");
     } else {
