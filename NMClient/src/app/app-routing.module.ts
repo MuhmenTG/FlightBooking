9234bc21a41061d0e1f87afefc8f766100e1a082
Nicklas Sams <nicklasvsams@gmail.com>
2023-06-12 10:47:46 +0200

    Added route and temporary link to admin page

--- NMClient/src/app/app-routing.module.ts
+++ NMClient/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchFlightsComponent } from './search-flights/search-flights.component';
 import { SearchHotelsComponent } from './search-hotels/search-hotels.component';
 import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: '', component: SearchFlightsComponent },
   { path: 'hotels', component: SearchHotelsComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent}
 ];
 
 @NgModule({
