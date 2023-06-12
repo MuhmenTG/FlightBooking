6b2a82904698ef5c56abfb1a1492d3ba5a5890f9
Nicklas Sams <nicklasvsams@gmail.com>
2023-06-12 10:48:43 +0200

    temp service

--- NMClient/src/app/_services/agent.service.ts
+++ NMClient/src/app/_services/agent.service.ts
@@ -10,51 +10,51 @@ import { EnquiryResponse } from '../_models/Enquiries/EnquiryResponse';
 export class AgentService extends HttpService {
   private apiUrl = environment.apiUrl + "/travelAgent";
 
-  getAllFlightBookings(): Observable<PH> {
-    return this.http.get<PH>(this.apiUrl + "/getAllFlightBookings");
-  }
+  // getAllFlightBookings(): Observable<PH> {
+  //   return this.http.get<PH>(this.apiUrl + "/getAllFlightBookings");
+  // }
 
-  getAllHotelBookings(): Observable<PH> {
-    return this.http.get<PH>(this.apiUrl + "/getAllHotelBookings");
-  }
+  // getAllHotelBookings(): Observable<PH> {
+  //   return this.http.get<PH>(this.apiUrl + "/getAllHotelBookings");
+  // }
 
-  getBooking(id: number): Observable<PH> {
-    return this.http.get<PH>(this.apiUrl + "/getBooking/" + id);
-  }
+  // getBooking(id: number): Observable<PH> {
+  //   return this.http.get<PH>(this.apiUrl + "/getBooking/" + id);
+  // }
 
-  cancelHotelBooking(id: number): Observable<AccountResponse> {
-    return this.http.put<AccountResponse>(this.apiUrl + "/cancelHotel/" + id, this.httpOptions);
-  }
+  // cancelHotelBooking(id: number): Observable<AccountResponse> {
+  //   return this.http.put<AccountResponse>(this.apiUrl + "/cancelHotel/" + id, this.httpOptions);
+  // }
 
-  cancelFlightBooking(id: number): Observable<AccountResponse> {
-    return this.http.put<AccountResponse>(this.apiUrl + "/cancelFlight/" + id, this.httpOptions);
-  }
+  // cancelFlightBooking(id: number): Observable<AccountResponse> {
+  //   return this.http.put<AccountResponse>(this.apiUrl + "/cancelFlight/" + id, this.httpOptions);
+  // }
 
-  sendBookingToCustomer(): Observable<AccountResponse> {
-    return this.http.post<AccountResponse>(this.apiUrl + "/sendBooking", this.httpOptions);
-  }
+  // sendBookingToCustomer(): Observable<AccountResponse> {
+  //   return this.http.post<AccountResponse>(this.apiUrl + "/sendBooking", this.httpOptions);
+  // }
 
-  answerEnquiry(): Observable<AccountResponse> {
-    return this.http.post<AccountResponse>(this.apiUrl + "/answerUserEnquiry", this.httpOptions);
-  }
+  // answerEnquiry(): Observable<AccountResponse> {
+  //   return this.http.post<AccountResponse>(this.apiUrl + "/answerUserEnquiry", this.httpOptions);
+  // }
 
-  setEnquiryStatus(id: number): Observable<AccountResponse> {
-    return this.http.post<AccountResponse>(this.apiUrl + "/setUserEnquiryStatus/" + id, this.httpOptions);
-  }
+  // setEnquiryStatus(id: number): Observable<AccountResponse> {
+  //   return this.http.post<AccountResponse>(this.apiUrl + "/setUserEnquiryStatus/" + id, this.httpOptions);
+  // }
 
-  deleteEnquiryStatus(id: number): Observable<AccountResponse> {
-    return this.http.delete<AccountResponse>(this.apiUrl + "/removeUserEnquiry/" + id);
-  }
+  // deleteEnquiryStatus(id: number): Observable<AccountResponse> {
+  //   return this.http.delete<AccountResponse>(this.apiUrl + "/removeUserEnquiry/" + id);
+  // }
 
-  getAllEnquiries(): Observable<EnquiryResponse[]> {
-    return this.http.get<EnquiryResponse[]>(this.apiUrl + "/getAllUserEnquries");
-  }
+  // getAllEnquiries(): Observable<EnquiryResponse[]> {
+  //   return this.http.get<EnquiryResponse[]>(this.apiUrl + "/getAllUserEnquries");
+  // }
 
-  getSpecificEnquiry(id: number): Observable<EnquiryResponse> {
-    return this.http.get<EnquiryResponse>(this.apiUrl + "/getSpecificUserEnquiry/" + id);
-  }
+  // getSpecificEnquiry(id: number): Observable<EnquiryResponse> {
+  //   return this.http.get<EnquiryResponse>(this.apiUrl + "/getSpecificUserEnquiry/" + id);
+  // }
 
-  editAccount(): Observable<PH> {
-    return this.http.post<PH>(this.apiUrl + "/editAgentDetails", this.httpOptions);
-  }
+  // editAccount(): Observable<PH> {
+  //   return this.http.post<PH>(this.apiUrl + "/editAgentDetails", this.httpOptions);
+  // }
 }
