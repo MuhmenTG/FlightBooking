0bcdf58009e0fc5467aa82655bc134b6f2c056e0
Nicklas Sams <nicklasvsams@gmail.com>
2023-06-12 10:49:55 +0200

    Initial Admin code

--- NMClient/src/app/admin/admin.component.ts
+++ NMClient/src/app/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../_services/admin.service';
+import { NgForm } from '@angular/forms';
+import { AccountRequest } from '../_models/Employees/AccountRequest';
 
 @Component({
   selector: 'app-admin',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class AdminComponent {
 
+  model: AccountRequest = {email: "", firstName: "", lastName: "", status: "", isAdmin: 0, isAgent: 1};
+
+  constructor(private _adminService: AdminService){};
+
+  submitForm(form: NgForm) {
+    if (!form.valid) {
+      return alert("Form is not valid. Try again.");
+    } else {
+      
+      console.log(this.model);
+      // this._adminService.createAccount(this.model).subscribe(response => {
+      //   console.log(response);
+      // })
+    }
+  }
 }
