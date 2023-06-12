255e7bbae6876584ac8660be07bba66a6ecc1432
Nicklas Sams <nicklasvsams@gmail.com>
2023-06-12 14:29:15 +0200

    Get list of accounts on init of admin page

--- NMClient/src/app/admin/admin.component.ts
+++ NMClient/src/app/admin/admin.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../_services/admin.service';
 import { NgForm } from '@angular/forms';
 import { AccountRequest } from '../_models/Employees/AccountRequest';
+import { AccountResponse } from '../_models/Employees/AccountResponse';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit{
 
   model: AccountRequest = {email: "", firstName: "", lastName: "", status: "", isAdmin: 0, isAgent: 1};
+  accounts : AccountResponse [] = [];
 
   constructor(private _adminService: AdminService){};
+
+  ngOnInit(): void {
+    // this._adminService.getListOfAccounts().subscribe(response => this.accounts = response);
+  }
 
   submitForm(form: NgForm) {
     if (!form.valid) {
