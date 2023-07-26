428579aeb4d182d86ced11f01fdc4dc581c914fc
MuhmenTG <mpe@nota.dk>
2023-07-26 20:42:44 +0200

    fix (sendEmail) use of another attachment

--- bookingportal/app/Mail/SendEmailService.php
+++ bookingportal/app/Mail/SendEmailService.php
@@ -4,33 +4,36 @@ namespace App\Mail;
 
 use Exception;
 use Illuminate\Mail\Mailables\Attachment;
-use SendGrid;
-use SendGrid\Mail\Mail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Message;
+use SendGrid\Mail\Mail as SendGridMail;
 
 class SendEmailService implements ISendEmailService
 {
     /**
     * {@inheritDoc}
     */
-    public function sendEmailWithAttachments(string $recipientName, string $recipientEmail, string $subject, string $text, array $attachments = null) : bool{
-        $email = new Mail();
-        $email->setFrom('nmflights-costumerservice@hotmail.com', 'N&M flights booking');
+    public function sendEmailWithAttachments(string $recipientName, string $recipientEmail, string $subject, string $text, array $attachments = null) : bool
+    {
+        $email = new SendGridMail();
+        $email->setFrom("nmflights-costumerservice@hotmail.com", "N&M flights booking");
         $email->setSubject($subject);
         $email->addTo($recipientEmail, $recipientName);
         $email->addContent("text/plain", $text);
     
-        if(!is_null($attachments)){   
+        if (!is_null($attachments)) {
             foreach ($attachments as $attachment) {
-                $attachmentFile = new Attachment();
-                $attachmentFile->setContent(base64_encode(file_get_contents($attachment->getRealPath())));
-                $attachmentFile->setType($attachment->getClientMimeType());
-                $attachmentFile->setFilename($attachment->getClientOriginalName());
-                $attachmentFile->setDisposition('attachment');
-                $email->addAttachment($attachmentFile);
+                $fileContent = base64_encode(file_get_contents($attachment->getRealPath()));
+                $email->addAttachment(
+                    $fileContent,
+                    $attachment->getClientMimeType(),
+                    $attachment->getClientOriginalName(),
+                    "attachment"
+                );
             }
         }
     
-        $sendgrid = new SendGrid(getenv('SEND_GRID_API_KEY'));
+        $sendgrid = new \SendGrid(getenv('SEND_GRID_API_KEY'));
         try {
             $sendgrid->send($email);
             return true;
@@ -38,6 +41,6 @@ class SendEmailService implements ISendEmailService
             return false;
         }
     }
-    
-    
 }
+    
+    
