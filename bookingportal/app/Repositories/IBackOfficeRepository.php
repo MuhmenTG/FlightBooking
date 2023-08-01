daeb22dee183c6a7d8e159cd8491e61dc565a685
MuhmenTG <97025113+MuhmenTG@users.noreply.github.com>
2023-08-01 09:59:23 +0200

    Update IBackOfficeRepository.php

--- bookingportal/app/Repositories/IBackOfficeRepository.php
+++ bookingportal/app/Repositories/IBackOfficeRepository.php
@@ -22,6 +22,8 @@ interface IBackOfficeRepository {
     public function getActivatedAgents(): Collection;
 
     public function getSpecificFaq(int $faqId) : ?Faq;
+    
+    public function createOrUpdateFaq(string $question, string $answer, int $faqId = null): Faq;
 
     public function getDeactivatedAgents(): array;
 
