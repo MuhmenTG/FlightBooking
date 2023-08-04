ce13c52fa12325e54678c18d4d8ae96b08aa677f
MuhmenTG <97025113+MuhmenTG@users.noreply.github.com>
2023-08-04 11:44:05 +0200

    Update FlightSearchRequest.php

--- bookingportal/app/Http/Requests/FlightSearchRequest.php
+++ bookingportal/app/Http/Requests/FlightSearchRequest.php
@@ -6,15 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class FlightSearchRequest extends FormRequest
 {
-      */
+      
     public function authorize(): bool
     {
         return true;
-    }  */
-    public function authorize(): bool
-    {
-        return true;
-    }
+    }  
     
     /**
      * Get the validation rules that apply to the request.
