ccedc82261f883c677be6b8d18f839de48439b92
MuhmenTG <97025113+MuhmenTG@users.noreply.github.com>
2023-08-04 11:44:30 +0200

    Update LoginRequest.php

--- bookingportal/app/Http/Requests/LoginRequest.php
+++ bookingportal/app/Http/Requests/LoginRequest.php
@@ -5,7 +5,13 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
-{    /**
+{
+      */
+    public function authorize(): bool
+    {
+        return true;
+    }
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
