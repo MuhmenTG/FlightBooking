cf709f979bfbe0a24ad1d8c87b7e93f42c08ff13
Nicklas Sams <nicklasvsams@gmail.com>
2023-08-17 10:57:49 +0200

    Slight change to model

--- NMClient/src/app/_models/Flights/SearchFlightsRequest.ts
+++ NMClient/src/app/_models/Flights/SearchFlightsRequest.ts
@@ -2,8 +2,10 @@ export interface SearchFlightsRequest {
     travelType: number
     originLocationCode: string
     destinationLocationCode: string
+    departureDateVar?: Date | null
+    returnDateVar?: Date | null
     departureDate: string
-    returnDate: string
+    returnDate: string;
     travelClass: string
     adults: number
 }
