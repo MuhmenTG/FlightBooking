63c385c5b67d965a41f76dfc7bbc5bce34b134c9
Nicklas Sams <nicklasvsams@gmail.com>
2023-08-24 11:01:30 +0200

    Styling work

--- NMClient/src/app/agent/agent.component.ts
+++ NMClient/src/app/agent/agent.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AgentService } from '../_services/agent.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agent',
@@ -8,15 +9,21 @@ import { AgentService } from '../_services/agent.service';
 })
 export class AgentComponent implements OnInit {
   agentToken = sessionStorage.getItem('token');
+  role: boolean = false;
 
-  constructor(private _agentService: AgentService) { }
+  constructor(private _agentService: AgentService, private _router: Router) { }
 
   ngOnInit(): void {
-    this._agentService.setHttpOptionsAccount(this.agentToken!);
-    console.log(this.agentToken)
-
-    this._agentService.getAllFlightBookings().subscribe(response => {
-      console.log(response);
-    })
+    if (sessionStorage.getItem('role') == 'agent'){
+      this._agentService.setHttpOptionsAccount(this.agentToken!);
+      console.log(this.agentToken)
+  
+      this._agentService.getAllFlightBookings().subscribe(response => {
+        console.log(response);
+      })
+    }
+    else{
+      this._router.navigateByUrl('/');
+    }
   }
 }
