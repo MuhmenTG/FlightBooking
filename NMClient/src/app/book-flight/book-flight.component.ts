0093e742a7a6dae6013387a4aa5aba74fed14b31
Nicklas Sams <nicklasvsams@gmail.com>
2023-09-06 14:54:39 +0200

    Styling and layout updates

--- NMClient/src/app/book-flight/book-flight.component.ts
+++ NMClient/src/app/book-flight/book-flight.component.ts
@@ -15,20 +15,46 @@ export class BookFlightComponent {
   formSubmitted: boolean = false;
   model: CustomerInfo = { title: "Mr.", firstName: "", lastName: "", email: "", dateOfBirth: Date(), passengerType: "Adult" }
   bookingResponse: FlightBookingResponse = { bookingReference: "", success: false };
+
+  // Clean this up some day - Date class
+  currentYear = new Date().getFullYear();
+  currentMonth = new Date().getMonth();
+  currentDate = new Date().getDate();
+  isLoading: boolean = false;
+  flightBooked: boolean = false;
+  flightBookedHasResponse: boolean = false;
+  maxDate = new Date(this.currentYear, this.currentMonth, this.currentDate);
   constructor(private _flightService: FlightService) { }
 
   submitForm(form: NgForm) {
     if (!form.valid) {
       return alert("Form is not valid. Try again.");
     } else {
+      this.flightBooked = true;
+      this.isLoading = true;
       this.flightInfo.data.flightOffers[0].passengers = [];
       this.flightInfo.data.flightOffers[0].passengers.push(this.model);
-      this._flightService.getFlightConfirmation(this.flightInfo.data.flightOffers[0]).subscribe(response => {
-        this.bookingResponse = response;
-        this.formSubmitted = true;
-        return true;
+      this._flightService.getFlightConfirmation(this.flightInfo.data.flightOffers[0]).subscribe({
+        next: response => {
+          this.bookingResponse = response;
+          this.flightBookedHasResponse = true;
+          this.isLoading = false;
+          this.formSubmitted = true;
+          return true;
+        },
+        error: err => {
+          this.isLoading = false;
+          this.flightBooked = false;
+        }
       })
       return true;
+    }
+  }
+
+  ngAfterViewChecked() {
+    if (this.isLoading) {
+      let mySpinner = document.getElementById('spinner');
+      if (mySpinner != null) mySpinner.scrollIntoView({ behavior: 'smooth' })
     }
   }
 }
