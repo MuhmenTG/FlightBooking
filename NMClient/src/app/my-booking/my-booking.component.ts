3a7fe4dee6d37b44ac16210c5b7312b1f65711cd
Nicklas Sams <nicklasvsams@gmail.com>
2023-09-06 14:56:31 +0200

    Styling & layout updates, added loading spinner,

--- NMClient/src/app/my-booking/my-booking.component.ts
+++ NMClient/src/app/my-booking/my-booking.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PublicService } from '../_services/public.service';
 import { MyBookingRequest } from '../_models/Flights/myBookingRequest';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-bookings',
@@ -10,9 +11,10 @@ import { NgForm } from '@angular/forms';
 })
 export class MyBookingComponent implements OnInit {
 
+  isLoading: boolean = false;
   model: MyBookingRequest = { bookingReference: "" }
 
-  constructor(private _publicService: PublicService) { }
+  constructor(private _publicService: PublicService, private _router: Router) { }
 
   ngOnInit(): void {
     // this._publicService.retrieveBooking("DIVE4D").subscribe(response => {
@@ -24,9 +26,18 @@ export class MyBookingComponent implements OnInit {
     if (!form.valid) {
       return alert("Form is not valid. Try again.");
     } else {
+      this.isLoading = true;
       this._publicService.retrieveBooking(this.model.bookingReference).subscribe(response => {
-        console.log(response);
+        this._router.navigateByUrl('/bookingconfirmation', { state: response })
+        this.isLoading = false;
       })
+    }
+  }
+
+  ngAfterViewChecked() {
+    if (this.isLoading) {
+      let mySpinner = document.getElementById('spinner');
+      if (mySpinner != null) mySpinner.scrollIntoView({ behavior: 'smooth' })
     }
   }
 }
